bf69d3ae165c134054261dbf0c480dc4c0b18470
Gaskov Vadim <vadik.gaskoff@yandex.ru>
2025-02-05 16:55:51 +0800

    added fetch environments-models from server

--- src/pages/list-environment/ListEnvironment.js
+++ src/pages/list-environment/ListEnvironment.js
@@ -1,16 +1,36 @@
 import "./ListEnvironment.css";
 import NameEquipment from "../../components/list-environment/name-equipment/NameEquipment";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 const ListEnvironment = () => {
+    const [environments, setEnvironments] = useState([]);
+
+    useEffect(() => {
+        const fetchEquipments = async () => {
+            try {
+                const response = await fetch("http://localhost:5065/api/environment-model/get-all");
+                if (!response.ok)
+                    throw new Error('Ошибка при получении данных');
+
+                const data = await response.json();
+                setEnvironments(data);
+            }
+            catch (error){
+                console.error(error)
+            }
+        }
+
+        fetchEquipments();
+    }, []);
+
     return(
         <>
             <h2>Список оборудования</h2>
             <div className="list-equipment">
                 <ul>
-                    <NameEquipment title="Дефибриллятор" ></NameEquipment>
-                    <NameEquipment title="Дефибриллятор" ></NameEquipment>
-                    <NameEquipment title="Дефибриллятор" ></NameEquipment>
-                    <NameEquipment title="Дефибриллятор" ></NameEquipment>
+                    {environments.map(equipment =>
+                        <NameEquipment key={equipment.id} id={equipment.id} title={equipment.name} />
+                    )}
                 </ul>
             </div>
         </>
