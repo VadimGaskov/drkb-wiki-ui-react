b28cf898b62deea3cd117b5715eea72cd495855b
Gaskov Vadim <vadik.gaskoff@yandex.ru>
2025-05-29 10:44:28 +0800

    fixed active sidebar element

--- src/layouts/MainWikiLayout/_components/Sidebar/sidebar-element/SidebarElement.js
+++ src/layouts/MainWikiLayout/_components/Sidebar/sidebar-element/SidebarElement.js
@@ -1,14 +1,13 @@
-import {Link, useLocation} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./SidebarElement.css";
+
 const SidebarElement = ({ img, path, label }) => {
-    const location = useLocation();
-    const isActive = location.pathname.includes(path);
     return (
-        <li className={`sidebar-element ${isActive ? "active-element" : ""}`}>
-            <Link to={path} className="sidebar-img-wrapper">
+        <li className="sidebar-element">
+            <NavLink to={path} className="sidebar-img-wrapper">
                 <img className="img-nav" src={img} alt={label} />
                 <span className="sidebar-label">{label}</span>
-            </Link>
+            </NavLink>
         </li>
     );
 };
--- src/pages/AdminPanel/_components/AdminNavMenu/AdminNavMenu.js
+++ src/pages/AdminPanel/_components/AdminNavMenu/AdminNavMenu.js
@@ -1,13 +1,14 @@
+import { NavLink } from "react-router-dom";
 import "./AdminNavMenu.css";
-import {Link} from "react-router-dom";
+
 const AdminNavMenu = () => {
-    return(
-        <div className={"admin-nav"}>
-            <Link to={"/admin/education"} className={"admin-nav-link"}>Образование</Link>
-            <Link to={"/admin/equipments"}>Оборудование</Link>
-            <Link to={"/admin/users-management"}>Управление пользователями</Link>
+    return (
+        <div className="admin-nav">
+            <NavLink to="/admin/education" className="admin-nav-link">Образование</NavLink>
+            <NavLink to="/admin/equipments" className="admin-nav-link">Оборудование</NavLink>
+            <NavLink to="/admin/users-management" className="admin-nav-link">Управление пользователями</NavLink>
         </div>
     );
-}
+};
 
 export default AdminNavMenu;
